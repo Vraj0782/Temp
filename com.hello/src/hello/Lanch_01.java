47f5b1f11ded22490cf98a1bec1b35e98b2d5781
Vinu <Vinu@DESKTOP-NPFP78G>
2023-08-31 08:28:03 +0530

    Build_03

---
+++ com.hello/src/hello/Lanch_01.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Lanch_01 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/login/");
+		System.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
+		WebDriver driver1=new FirefoxDriver();
+		driver1.get("https://www.amazon.in/");
+	//	)
+	}
+
+}
